478b21509df9d8bbbe17f062408e54c227cdbfbe
HenriqueContini <henrique.coutine@gmail.com>
2023-12-26 17:24:21 -0300

    Projecao dos dados do autor com DTO

---
+++ src/main/java/com/estudos/workshop_spring_mongo/dto/AuthorDTO.java
@@ -0,0 +1,32 @@
+package com.estudos.workshop_spring_mongo.dto;
+
+import com.estudos.workshop_spring_mongo.domain.User;
+
+public class AuthorDTO {
+    private String id;
+    private String name;
+
+    public AuthorDTO() {
+    }
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
